67a152c88c6d0933c7839ec6fd70ca30ed1d2792
Alexis Stephan <alexis-stephan@hotmail.fr>
2022-09-13 20:30:05 +0200

    add an alumni

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -4,7 +4,7 @@ import Typed from "typed.js"
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ["Batch #916", "Amal, Morad, Nzila, Houssam and Yassine, AMIIINE TOOO"],
+      strings: ["Batch #916", "Amal, Morad, Nzila, Jihane, Houssam and Yassine, AMIIINE TOOO"],
       typeSpeed: 50,
       loop: true
     })
